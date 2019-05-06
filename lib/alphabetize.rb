7007241661937d63a6f82df2a54535f72ef93b78
tommaclean <tom.j.maclean@gmail.com>
2019-05-05 20:51:15 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(sentences_array)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      alphabet.index(character)
+    end
+  end
 end
